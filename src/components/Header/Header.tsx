3cd16acf7e8b72a18604cdc395f7a967a080c922
Masoud bahamin <bahaminwp@gmail.com>
2024-10-18 13:22:18 +0300

    test checkly

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -120,7 +120,7 @@ function Header() {
                             </Link>
                         </div>
                         <div className="hidden sm:block relative group rounded-full mb-1 md:mb-0" >
-                            <Link to={"/"} className=" w-11 h-11 bg-white rounded-lg text-bold hover:text-primary flex items-center justify-center gap-2 relative" >
+                            <Link to={"/profile"} className=" w-11 h-11 bg-white rounded-lg text-bold hover:text-primary flex items-center justify-center gap-2 relative" >
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-[22px] h-[22px]">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
                                 </svg>
